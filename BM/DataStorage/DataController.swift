05e0f128e16bfa5eb171822d00a84f5bf4764ca8
Nikita S <snikitasergeevich@gmail.com>
2022-10-27 20:05:32 +0300

    created persistence add tables

---
+++ BM/DataStorage/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataStorage.swift
+//  BM
+//
+//  Created by NikitaSergeevich on 27/10/2022.
+//
+
+import Foundation
+
+struct DataController {
+    
+}
+
+// MARK: - DataEditor -- for edit data
+// MARK: - DataCreator -- for create data
+// MARK: - DataProvider -- for get data
+// MARK: - Syncronizer -- for provide actual data
+// MARK: - Updator -- for change updated data
+// MARK: - Newsler -- for message ViewModel about updated data
+// MARK: - RequestHandler -- for handler request from ViewModel
+
+
